4aa2f10bb608d358f83e98852bb4dfb925d4e3ff
erwinmarots-code <erwinmarots@gmail.com>
2026-09-01 21:27:53 +0800

    Fix: move assets to public/assets and update app.blade.php

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Albatros Logistik</title>
-    <link rel="stylesheet" href="{{ asset('build/assets/main-BkaFKvsj.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/main-BkaFKvsj.css') }}">
 </head>
 <body>
     <div id="app"></div>
-    <script type="module" src="{{ asset('build/assets/main-DW31NBAo.js') }}"></script>
+    <script type="module" src="{{ asset('assets/main-DW31NBAo.js') }}"></script>
 </body>
 </html>
