9fa569cec0556149844ee7259a73aae2c3b4efc2
erwinmarots-code <erwinmarots@gmail.com>
2026-09-01 19:02:30 +0800

    Update app.blade.php with correct asset paths

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Albatros Logistik</title>
-    <link rel="stylesheet" href="{{ asset('build/assets/app-B0-YuHJH.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/assets/index-BkaFKvsj.css') }}">
 </head>
 <body>
     <div id="app"></div>
-    <script type="module" src="{{ asset('build/assets/app-DUr89oQr.js') }}"></script>
+    <script type="module" src="{{ asset('dist/assets/index-DwDjOfcI.js') }}"></script>
 </body>
 </html>
