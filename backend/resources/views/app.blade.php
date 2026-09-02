80fae4131d9cc5338cc8077f439748dff59731a9
erwinmarots-code <erwinmarots@gmail.com>
2026-09-02 22:26:59 +0800

    Fix: update asset files, hapus import gambar, hardcode file terbaru

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -5,14 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Albatros Logistik</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    
-    {{-- 🔥 LOAD CSS UTAMA --}}
-    <link rel="stylesheet" href="{{ asset('assets/main-C9lmkiG4.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/main-C9lmkiG4.css') }}">
 </head>
 <body>
     <div id="app"></div>
-    
-    {{-- 🔥 LOAD JS UTAMA --}}
-    <script type="module" src="{{ asset('assets/main-CXltJDqU.js') }}"></script>
+    <script type="module" src="{{ asset('assets/js/main-ChJ7I-1d.js') }}"></script>
 </body>
 </html>
